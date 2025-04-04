a782f29c23f25163a8bda3f139d5aa487222b7b5
sho27ny <sho27nyo1@gmail.com>
2025-04-04 13:12:04 +0900

    手順6 出品した商品の情報をテーブルに保存できるようにする

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,5 +13,5 @@ class Item < ApplicationRecord
   validates :description, presence: true
   validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   validates :category_id, :condition_id, :shipping_cost_id, :prefecture_id, :shipping_time_id, 
-  numericality: { other_than: 1, message: "can't be blank" }
+  presence: true, numericality: { other_than: 1, message: "can't be blank" }
 end
