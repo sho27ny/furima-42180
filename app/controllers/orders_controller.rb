700c9be8dd8c58502939c2f720f84b58c21cfad3
sho27ny <sho27nyo1@gmail.com>
2025-04-14 12:25:33 +0900

    フォームの情報を保存できるようにする

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,17 +2,21 @@ class OrdersController < ApplicationController
   before_action :set_item, only: [:index, :create]
 
   def index
+    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
   end
 
   def create
     @order_address = OrderAddress.new(order_address_params)
-    @order_address.item = @item
-    @order_address.user = current_user
-
+    @order_address.item_id = @item.id
+    @order_address.user_id = current_user.id
+  
     if @order_address.valid?
-      @order_address.save
-      redirect_to root_path
+      if @order_address.save
+        redirect_to root_path, notice: '購入が完了しました'
+      else
+        render :index
+      end
     else
       render :index
     end
@@ -25,6 +29,6 @@ class OrdersController < ApplicationController
   end
 
   def order_address_params
-    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :address, :building_name, :phone_number)
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number,:token)
   end
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -9,7 +9,7 @@ class OrderAddress
   with_options presence: true do
     validates :user_id
     validates :item_id
-    validates :token
+    #validates :token
     validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/ }
     validates :prefecture_id, numericality: { other_than: 0 }
     validates :city
@@ -18,16 +18,37 @@ class OrderAddress
   end
 
   def save
-    order = Order.create(user_id: user_id, item_id: item_id)
-
-    Address.create(
-      postal_code: postal_code,
-      prefecture_id: prefecture_id,
-      city: city,
-      house_number: house_number,
-      building_name: building_name,
-      phone_number: phone_number,
-      order_id: order.id
-    )
+    ActiveRecord::Base.transaction do
+      order = Order.create(user_id: user_id, item_id: item_id)
+      puts " Order created: #{order.id}" if order.persisted?
+  
+      unless order.persisted?
+        puts " Order保存に失敗しました: #{order.errors.full_messages}"
+        raise ActiveRecord::Rollback
+      end
+  
+      address = Address.create(
+        postal_code: postal_code,
+        prefecture_id: prefecture_id,
+        city: city,
+        house_number: house_number,
+        building_name: building_name,
+        phone_number: phone_number,
+        order_id: order.id
+      )
+      puts " Address created: #{address.id}" if address.persisted?
+  
+      unless address.persisted?
+        puts " Address保存に失敗しました: #{address.errors.full_messages}"
+        raise ActiveRecord::Rollback
+      end
+  
+      # ✅ 成功した場合は true を返す
+      return true
+    end
+  
+    # トランザクションが失敗したときは false を返す
+    false
   end
+  
 end
