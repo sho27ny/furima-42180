741048c441b7dca2f049687cad51c4fecf704847
sho27ny <sho27nyo1@gmail.com>
2025-04-16 17:02:40 +0900

    バリデーションの被り削除

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,7 +1,3 @@
 class Address < ApplicationRecord
   belongs_to :order
-  validates :postal_code, presence: true, format: { with: /\A\d{3}-\d{4}\z/ }
-  validates :prefecture_id, numericality: { other_than: 0 }
-  validates :city, :house_number, :phone_number, presence: true
-  validates :phone_number, format: { with: /\A\d{10,11}\z/ }
 end
